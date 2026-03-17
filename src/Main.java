f4e60918a0548e2e38e0cd38fed586f8268af648
yanlinyan <yan.linyan06@gmail.com>
2026-03-17 14:20:15 +0800

    hello just testing - linyan

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main (String [] args)
     {
             System.out.println("Combat Arena Start!");
+            System.out.println("Linyan testing");
         }
     }
