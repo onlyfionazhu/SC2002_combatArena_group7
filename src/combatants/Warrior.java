6b618809f5889fa1e89d97b192c36e6cd00a0be3
zhufeiyang <onlyfionazhu@gmail.com>
2026-03-30 15:05:39 +0800

    mingku 2.0 - linyan

--- src/combatants/Warrior.java
+++ src/combatants/Warrior.java
@@ -13,9 +13,8 @@ import java.util.List;
  */
 
 public class Warrior extends Player {
-
-    public Warrior(String name) {
-        super(name, 260, 40, 20, 30);
+    public Warrior() {
+        super("Warrior", 260, 40, 20, 30);
     }
 
     public ActionResult useSpecialSkill(List<Combatant> targets) {
